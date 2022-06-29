e500d17c6c2b845da47f2754858dea638aa1bc79
bridget5329 <54876498+bridget5329@users.noreply.github.com>
2022-06-29 11:12:29 +0800

    Win_interview

---
+++ ForInterview/InterView.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public partial class InterView : System.Web.UI.Page
+{
+    public class Person
+    {
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+    }
+}
